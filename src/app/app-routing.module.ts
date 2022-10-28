5c88d5a03e5ff5c04d72b03ecffea9f65996ccd5
YodDigital <yoddigital@gmail.com>
2022-10-28 13:13:44 +0200

    cloudKitchenHomePg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './Pages/home/home.component';
+import {MenuComponent} from './Pages/menu/menu.component';
+import {ContactComponent} from './Pages/contact/contact.component';
+import {AboutComponent} from './Pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'menu', component: MenuComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharedPages/navbar/navbar.component';
+import { FooterComponent } from './sharedPages/footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { MenuComponent } from './Pages/menu/menu.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { AboutComponent } from './Pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
